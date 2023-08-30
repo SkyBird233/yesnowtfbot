da96994aa0f8119997766c60aeba5d800ad0e472
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2023-08-31 00:33:44 +0800

    Add function: roll

--- src/worker.js
+++ src/worker.js
@@ -17,8 +17,9 @@ export default {
 
 		const yesnowtfData = await fetch(yesnowtfApi).then(response => response.json());
 
+		const command = requestJson.message.text.split(" ");
 
-		if (requestJson.message.text === "/start") {
+		if (command[0] === "/start") {
 			const data = {
 				"chat_id": requestJson.message.chat.id,
 				"text": "*\\*WIP\\**\nType /gif to get a random gif from yesno\\.wtf",
@@ -26,12 +27,41 @@ export default {
 			}
 			console.log(await sendTgMessage(data, env.TG_BOT_TOKEN));
 		}
-		else if (requestJson.message.text === "/gif") {
+		else if (command[0] === "/gif") {
 			const data = {
 				"chat_id": requestJson.message.chat.id,
 				"animation": yesnowtfData.image,
 			}
 			console.log(await sendTgMessage(data, env.TG_BOT_TOKEN, "sendAnimation"));
+		}
+		else if (command[0] === "/help") {
+			const data = {
+				"chat_id": requestJson.message.chat.id,
+				"text": "*Commands:*\n/gif \\- Get a random GIF from yesno\\.wtf \\.\n/roll \\- Roll a random int in range\\. Default is \\[1,6\\], one argument to change the max value, two arguments to change min and max\\. Example: `/roll 10 20`",
+				"parse_mode": "MarkdownV2",
+			}
+			console.log(await sendTgMessage(data, env.TG_BOT_TOKEN));
+		}
+		else if (command[0] === "/roll") {
+			const data = {
+				"chat_id": requestJson.message.chat.id,
+				"text": "",
+			}
+
+			if (command.length === 1) {
+				data.text = "[1,6] -> " + (Math.floor(Math.random() * 6) + 1);
+			}
+			else if (command.length === 2) {
+				data.text = "[1," + command[1] + "] -> " + (Math.floor(Math.random() * command[1]) + 1);
+			}
+			else if (command.length === 3) {
+				data.text = "[" + command[1] + "," + command[2] + "] -> " + (Math.floor(Math.random() * (command[2] - command[1] + 1)) + parseInt(command[1]));
+			}
+			else {
+				data.text = "Invalid command";
+			}
+
+			console.log(await sendTgMessage(data, env.TG_BOT_TOKEN));
 		}
 		return new Response();
 	},
